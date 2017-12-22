93862e17d7b8f367d860aa02c9013e256340b590
phugh <peter@phugh.es>
2017-12-22 20:38:11 +0000

    0.2.2

--- js/main.js
+++ js/main.js
@@ -1,56 +1,48 @@
-/* jshint globalstrict: true, browser: true, sub: true, esversion: 5, asi: true, -W041 */ /* global $, Materialize, Chart, FB */
-'use strict'; // eslint-disable-line
-var testNo = 0 // testNo becomes a count of previous tests in localStorage
-var storage = false // localStorage availability
+'use strict';
+var testNo = 0; // testNo becomes a count of previous tests in localStorage
+var storage = false; // localStorage availability
 
 /**
 * Handles Facebook score sharing
 * @function postToFeed
 */
-function postToFeed () {
+function postToFeed() {
   var obj = {
     method: 'feed',
     link: 'http://www.phugh.es/tech/permanaut/',
     description: 'I just got a wellbeing score of ' + $('.owRes').text() + '/10, what will you get?',
     picture: 'http://www.phugh.es/tech/permanaut/tile-wide.png',
-    name: 'PERMAnaut Wellbeing Quiz'
-  }
-  FB.ui(obj)
+    name: 'PERMAnaut Wellbeing Quiz',
+  };
+  FB.ui(obj);
 }
 
 /**
 * Test if localStorage is available
 * @function lsTest
-* @return {bool} true or false
+* @return {boolean}
 */
-function lsTest () {
-  var test = 'test'
+function lsTest() {
+  var test = 'test';
   try {
-    window.localStorage.setItem(test, test)
-    window.localStorage.removeItem(test)
-    if (storage != null) storage = true
-    return true
+    window.localStorage.setItem(test, test);
+    window.localStorage.removeItem(test);
+    if (!storage) storage = true;
+    return true;
   } catch (e) {
-    if (storage != null) storage = false
-    return false
+    if (storage) storage = false;
+    return false;
   }
 }
 
 /**
 * Sum input slider values
 * @function calcSliders
-* @param  {type} x {description}
+* @param  {Element} x slider element
 * @return {Number} Sum
 */
-function calcSliders (x) {
-  var sliderValues = []
-  var total = 0
-  var index, len
-  sliderValues.push(parseInt($(x).val(), 10))
-  for (index = 0, len = sliderValues.length; index < len; ++index) {
-    total += sliderValues[index]
-  }
-  return total
+function calcSliders(x) {
+  return parseInt($(x).val(), 10);
 }
 
 /**
@@ -58,8 +50,8 @@ function calcSliders (x) {
 * @function updatevalues
 * @param  {object} x slider DOM element
 */
-function updatevalues (x) { // eslint-disable-line
-  $(x).next('span').next('span').text(x.value)
+function updatevalues(x) {
+  $(x).next('span').next('span').text(x.value);
 }
 
 /**
@@ -67,30 +59,29 @@ function updatevalues (x) { // eslint-disable-line
 * @function nextBlock
 * @param  {object} x Anchor DOM element
 */
-function nextBlock (x) { // eslint-disable-line
-  // hide the current block, find the next one and show it
-  var thisBlock = $(x).closest('div.block')
-  $(thisBlock).hide()
-  $(thisBlock).next('div.block').show()
+function nextBlock(x) {
+  var thisBlock = $(x).closest('div.block');
+  $(thisBlock).hide();
+  $(thisBlock).next('div.block').show();
 }
 
 /**
 * Load and display previous scores from localStorage
 * @function getPreviousScores
 */
-function getPreviousScores () {
+function getPreviousScores() {
   if (testNo !== 0) {
     for (var i = 0; i < testNo; i++) {
-      var retrievedObject = window.localStorage.getItem('PNS' + (i + 1))
-      var q = JSON.parse(retrievedObject)
+      var retrievedObject = window.localStorage.getItem('PNS' + (i + 1));
+      var q = JSON.parse(retrievedObject);
       $('.tbody').append('<tr><td>' + q.Name + '</td><td>' + q.Date +
                 '</td><td>' + q.P + '</td><td>' + q.N +
                 '</td><td>' + q.E + '</td><td>' + q.R +
                 '</td><td>' + q.M + '</td><td>' + q.A +
                 '</td><td>' + q.H + '</td><td>' + q.L +
-                '</td><td>' + q.O + '</td></tr>')
+                '</td><td>' + q.O + '</td></tr>');
     }
-    $('.prevResTable').show()
+    $('.prevResTable').show();
   }
 }
 
@@ -98,14 +89,15 @@ function getPreviousScores () {
 * Clear scores from localStorage
 * @function clearPreviousScores
 */
-function clearPreviousScores () { // eslint-disable-line
-  $('.tbody').empty()
-  $.each(window.localStorage, function (i, v) {
+function clearPreviousScores() {
+  $('.tbody').empty();
+  $('.prevResTable').hide();
+  $.each(window.localStorage, function(i, v) {
     if (i.match(/^PNS/)) {
-      window.localStorage.removeItem(i)
+      window.localStorage.removeItem(i);
     }
-  })
-  Materialize.toast('Previous scores cleared', 2500)
+  });
+  Materialize.toast('Previous scores cleared', 2500);
 }
 
 /**
@@ -113,96 +105,104 @@ function clearPreviousScores () { // eslint-disable-line
 * @function checkKey
 * @param  {event} e window.event
 */
-function checkKey (e) {
-  e = e || window.event
+function checkKey(e) {
+  e = e || window.event;
   if (e.keyCode === 39) {
-    var x = $('.btnNext:visible')
-    x.click()
+    var x = $('.btnNext:visible');
+    x.click();
   }
+}
+
+/**
+ * @function add
+ * @param  {Number} a
+ * @param  {Number} b
+ * @return {Number} sum
+ */
+function add(a, b) {
+  return a + b;
 }
 
 /**
 * Our main function
 * @function calculateScores
 */
-function calculateScores () {
+function calculateScores() {
   // define our containers
   var PERMAScores = {
-    'Positive Emotion': 0,
-    'Engagement': 0,
-    'Relationships': 0,
-    'Meaning': 0,
-    'Accomplishment': 0,
-    'Negative Emotion': 0,
-    'Health': 0,
-    'Loneliness': 0,
-    'Happy': 0
-  }
+    'Positive Emotion': [],
+    'Engagement': [],
+    'Relationships': [],
+    'Meaning': [],
+    'Accomplishment': [],
+    'Negative Emotion': [],
+    'Health': [],
+    'Loneliness': [],
+    'Happy': [],
+  };
 
   // get values from sliders, convert to readable number, and dump the results in the corresponding container then add them up
-  $('input[id^=P]').each(function () {
-    PERMAScores['Positive Emotion'] += calcSliders(this)
-  })
-  $('input[id^=E]').each(function () {
-    PERMAScores['Engagement'] += calcSliders(this)
-  })
-  $('input[id^=R]').each(function () {
-    PERMAScores['Relationships'] += calcSliders(this)
-  })
-  $('input[id^=M]').each(function () {
-    PERMAScores['Meaning'] += calcSliders(this)
-  })
-  $('input[id^=A]').each(function () {
-    PERMAScores['Accomplishment'] += calcSliders(this)
-  })
-  $('input[id^=N]').each(function () {
-    PERMAScores['Negative Emotion'] += calcSliders(this)
-  })
-  $('input[id^=H]').each(function () {
-    PERMAScores['Health'] += calcSliders(this)
-  })
+  $('input[id^=P]').each(function() {
+    PERMAScores['Positive Emotion'].push(calcSliders(this));
+  });
+  $('input[id^=E]').each(function() {
+    PERMAScores['Engagement'].push(calcSliders(this));
+  });
+  $('input[id^=R]').each(function() {
+    PERMAScores['Relationships'].push(calcSliders(this));
+  });
+  $('input[id^=M]').each(function() {
+    PERMAScores['Meaning'].push(calcSliders(this));
+  });
+  $('input[id^=A]').each(function() {
+    PERMAScores['Accomplishment'].push(calcSliders(this));
+  });
+  $('input[id^=N]').each(function() {
+    PERMAScores['Negative Emotion'].push(calcSliders(this));
+  });
+  $('input[id^=H]').each(function() {
+    PERMAScores['Health'].push(calcSliders(this));
+  });
 
-  // these sliders are just single values so no containers are required
-  PERMAScores['Loneliness'] = parseInt($('#LON').val(), 10)
-  PERMAScores['Happy'] = parseInt($('#Z1').val(), 10)
+  // these sliders are just single values
+  PERMAScores['Loneliness'] = parseInt($('#LON').val(), 10);
+  PERMAScores['Happy'] = parseInt($('#Z1').val(), 10);
 
-  // calculate results to two decimal places
+  // calculate overall wellbeing score
+  var owTotal = PERMAScores['Positive Emotion'].concat(
+      PERMAScores['Engagement'],
+      PERMAScores['Relationships'],
+      PERMAScores['Meaning'],
+      PERMAScores['Accomplishment']
+  );
+  PERMAScores['Overall'] = parseFloat((owTotal.reduce(add, PERMAScores['Happy']) / 16).toFixed(2));
+
+  // calculate averages to two decimal places
   for (var key in PERMAScores) {
     if (PERMAScores.hasOwnProperty(key)) {
-      if (key === 'Happy' || key === 'Loneliness') { // HERE
-        PERMAScores[key] = PERMAScores[key]
+      if (key === 'Happy' || key === 'Loneliness' || key === 'Overall') { // HERE
+        PERMAScores[key] = PERMAScores[key];
       } else {
-        PERMAScores[key] = parseFloat((PERMAScores[key] / 3).toFixed(2))
+        PERMAScores[key] = parseFloat((PERMAScores[key].reduce(add, 0) / 3).toFixed(2));
       }
     }
   }
 
-  // calculate overall wellbeing score
-  var owTotal =
-    PERMAScores['Positive Emotion'] +
-    PERMAScores['Engagement'] +
-    PERMAScores['Relationships'] +
-    PERMAScores['Meaning'] +
-    PERMAScores['Accomplishment'] +
-    PERMAScores['Happy']
-
-  PERMAScores['Overall'] = parseFloat((owTotal / 6).toFixed(2))
-
   // display text
-  $('.pRes').text(PERMAScores['Positive Emotion'])
-  $('.eRes').text(PERMAScores['Engagement'])
-  $('.rRes').text(PERMAScores['Relationships'])
-  $('.mRes').text(PERMAScores['Meaning'])
-  $('.aRes').text(PERMAScores['Accomplishment'])
-  $('.nRes').text(PERMAScores['Negative Emotion'])
-  $('.hRes').text(PERMAScores['Health'])
-  $('.lonRes').text(PERMAScores['Loneliness'])
-  $('.owRes').text(PERMAScores['Overall'])
+  $('.pRes').text(PERMAScores['Positive Emotion']);
+  $('.eRes').text(PERMAScores['Engagement']);
+  $('.rRes').text(PERMAScores['Relationships']);
+  $('.mRes').text(PERMAScores['Meaning']);
+  $('.aRes').text(PERMAScores['Accomplishment']);
+  $('.nRes').text(PERMAScores['Negative Emotion']);
+  $('.hRes').text(PERMAScores['Health']);
+  $('.lonRes').text(PERMAScores['Loneliness']);
+  $('.owRes').text(PERMAScores['Overall']);
 
   // hide the testCards and scroll to the top of the page
-  document.getElementById('quizPane').classList.add('hidden')
-  window.scrollTo(0, 0)
-  document.getElementById('resultsPane').classList.remove('hidden')
+  document.getElementById('quizPane').classList.add('hidden');
+  window.scrollTo(0, 0);
+  document.getElementById('resultsPane').classList.remove('hidden');
 
   // generate chart
   var dataArr = [
@@ -213,31 +213,31 @@ function calculateScores () {
     PERMAScores['Accomplishment'],
     PERMAScores['Negative Emotion'],
     PERMAScores['Health'],
-    PERMAScores['Overall']
-  ]
+    PERMAScores['Overall'],
+  ];
   var data = {
     labels: ['P', 'E', 'R', 'M', 'A', 'N', 'H', 'OW'],
     datasets: [{
       label: 'Your Results',
       backgroundColor: '#2196f3',
-      data: dataArr
-    }]
-  }
+      data: dataArr,
+    }],
+  };
   var dataCompare = {
     labels: ['P', 'E', 'R', 'M', 'A', 'N', 'H', 'OW'],
     datasets: [
       {
         label: 'Your Results',
         backgroundColor: '#2196f3',
-        data: dataArr
+        data: dataArr,
       },
       {
         label: 'Average',
         backgroundColor: '#00bcd4',
-        data: [6.79, 7.41, 6.99, 7.17, 7.37, 7.14, 4.49, 7.03]
-      }
-    ]
-  }
+        data: [6.79, 7.41, 6.99, 7.17, 7.37, 7.14, 4.49, 7.03],
+      },
+    ],
+  };
   var opts = {
     scales: {
       yAxes: [{
@@ -245,44 +245,44 @@ function calculateScores () {
           max: 10,
           min: 0,
           stepSize: 1,
-          beginAtZero: true
-        }
-      }]
-    }
-  }
+          beginAtZero: true,
+        },
+      }],
+    },
+  };
 
-  var ctx = document.getElementById('resultsChart').getContext('2d')
-  var myBarChart = null // eslint-disable-line
+  var ctx = document.getElementById('resultsChart').getContext('2d');
+  var myBarChart;
   if ($('#goCompare').prop('checked')) {
     myBarChart = new Chart(ctx, {
       type: 'bar',
       data: dataCompare,
-      options: opts
-    })
+      options: opts,
+    });
   } else {
     myBarChart = new Chart(ctx, {
       type: 'bar',
       data: data,
-      options: opts
-    })
-    $('#keyText').hide()
+      options: opts,
+    });
+    $('#keyText').hide();
   }
 
   // print results page header
-  var d = new Date()
-  var month = d.getMonth() + 1
-  var day = d.getDate()
+  var d = new Date();
+  var month = d.getMonth() + 1;
+  var day = d.getDate();
   var outputDate = (('' + day).length < 2 ? '0' : '') + day + '/' +
     (('' + month).length < 2 ? '0' : '') + month + '/' +
-    d.getFullYear()
+    d.getFullYear();
 
-  var inp = $('#nameInput').val()
-  var tinp = $.trim(inp)
+  var inp = $('#nameInput').val();
+  var tinp = $.trim(inp);
   if (tinp.length > 0) {
-    $('#topText').text('PERMA-P - ' + inp + "'s PERMA Profile - " + outputDate)
+    $('#topText').text(' - ' + tinp + '\'s PERMA Profile - ' + outputDate);
   } else {
-    $('#topText').text('PERMA-P - ' + 'PERMA Profile Results - ' + outputDate)
-    tinp = 'Anon'
+    $('#topText').text(' - ' + 'PERMA Profile Results - ' + outputDate);
+    tinp = 'Anon';
   }
 
   // save results to localStorage
@@ -299,44 +299,44 @@ function calculateScores () {
       'N': PERMAScores['Negative Emotion'],
       'H': PERMAScores['Health'],
       'L': PERMAScores['Loneliness'],
-      'O': PERMAScores['Overall']
-    }
-    window.localStorage.setItem('PNS' + (testNo += 1), JSON.stringify(lsd))
+      'O': PERMAScores['Overall'],
+    };
+    window.localStorage.setItem('PNS' + (testNo += 1), JSON.stringify(lsd));
   }
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   // hide all test blocks except the first
-  $('.block').not(':first').hide()
-  $('.prevResTable').hide()
+  $('.block').not(':first').hide();
+  $('.prevResTable').hide();
 
   // test localStorage availability
-  lsTest()
+  lsTest();
 
   // if localStorage is available, itterate test number
   if (storage === true) {
-    $.each(window.localStorage, function (i, v) {
+    $.each(window.localStorage, function(i, v) {
       if (i.match(/^PNS/)) {
-        testNo += 1
+        testNo += 1;
       }
-    })
-    getPreviousScores()
+    });
+    getPreviousScores();
   } else {
-    Materialize.toast('Offline storage unavailable. Score saving disabled.', 4000)
+    Materialize.toast('Offline storage unavailable. Score saving is disabled.', 4000);
   }
 
   // set some global chart options
-  Chart.defaults.global.responsive = true
+  Chart.defaults.global.responsive = true;
 
   // check for keypresses
-  document.onkeydown = checkKey
+  document.onkeydown = checkKey;
 
   // Facebook init
-  window.fbAsyncInit = function () {
+  window.fbAsyncInit = function() {
     FB.init({
       appId: '1096633653680137',
       xfbml: true,
-      version: 'v2.5'
-    })
-  }
-})
+      version: 'v2.5',
+    });
+  };
+});
